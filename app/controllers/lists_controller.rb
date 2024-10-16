579cd1e02e071cce8a7d48fc21fcdf2843b36510
Hamm-Coder <176797595+Hamm-Coder@users.noreply.github.com>
2024-10-16 16:08:51 +0000

    Second_Commit:Validation

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,24 +1,26 @@
 class ListsController < ApplicationController
+
   def new
     # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
     @list = List.new
   end
 
-  #以下を追加
   def create
     #1.&2. データを受取り新規登録するためのインスタンス作成
-    list = List.new(list_params)
+    @list = List.new(list_params)
     #3. データをデータベースに保存するためのsaveメソッド実行
-    list.save
-    #4 トップ画面へリダイレクト
-    redirect_to list_path(list.id)
+    if @list.save
+    #4-1 トップ画面へリダイレクト
+      redirect_to list_path(@list.id)
+    else
+    #4-2 指定アクション名で同一コントローラー内の別アクションのviewを表示する。
+      render :new
+    end
   end
-
 
   def index
     @lists = List.all
   end
-
 
   def show
     @list = List.find(params[:id])
@@ -34,10 +36,17 @@ class ListsController < ApplicationController
     redirect_to list_path(list.id)
   end
 
+  def destroy #destroy
+    list = List.find(params[:id])# ListModelから、Paramsで、idカラム指定の該当IDのレコードを1件取得
+    list.destroy #データ(レコード)削除
+    redirect_to '/lists' #リダイレクトlists View
+  end
 
   private
+
   # ストロングパラメータ
   def list_params
     params.require(:list).permit(:title, :body, :image)
   end
+
 end
