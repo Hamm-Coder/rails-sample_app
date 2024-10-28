2dfa6f8d52c8b9a04903f23f39c5069746eec54e
Hamm-Coder <176797595+Hamm-Coder@users.noreply.github.com>
2024-10-28 16:56:14 +0000

    FlashMessage

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,20 +1,24 @@
 class ListsController < ApplicationController
 
   def new
-    # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
+    # Viewへ渡すためのインスタンス変数に空のModelオブジェクト（List）を生成する。
     @list = List.new
   end
 
   def create
-    #1.&2. データを受取り新規登録するためのインスタンス作成
+    #1.&2. データを受取り、新規登録するためのインスタンス変数（Modelオブジェクト用のparams（List）、オブジェクト）を作成する。
     @list = List.new(list_params)
     #3. データをデータベースに保存するためのsaveメソッド実行
     if @list.save
-    #4-1 トップ画面へリダイレクト
+      #4-1.フラッシュメッセージを定義し、詳細画面へリダイレクト（登録完了し、トップ画面へリダイレクト）
+      flash[:notice] = "投稿に成功しました。"
       redirect_to list_path(@list.id)
+
     else
     #4-2 指定アクション名で同一コントローラー内の別アクションのviewを表示する。
+      flash.now[:alert] = "投稿に失敗しました。" #キーをalertに変更
       render :new
+
     end
   end
 
